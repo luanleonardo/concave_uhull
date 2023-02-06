b18de329b056c419c354498797bd1e661f27f57e
Luan <luan.moraes@loggi.com>
2023-02-06 20:03:06 -0300

    refac: remove unnecessary functions

--- tests/utils/test_graph.py
+++ tests/utils/test_graph.py
@@ -1,13 +1,9 @@
-from collections import defaultdict
-
 import pytest
 
 from concave_uhull.utils.geometry import euclidean_distance
 from concave_uhull.utils.graph import (
     Graph,
-    add_edge,
-    remove_edge,
-    shortest_path,
+    dijkstra_algorithm,
     shortest_path_algorithm,
 )
 
@@ -23,34 +19,6 @@ def square_edges():
         ((1.0, 1.0), (1.0, 0.0)),
         ((1.0, 0.0), (0.0, 0.0)),
     ]
-
-
-def test_add_edge(square_edges):
-    """
-    Test add edge to graph.
-    """
-    # create data structure for graph, edge weights and edges
-    graph = defaultdict(set)
-    weight = defaultdict(dict)
-
-    # define graph from edges
-    for source, target in square_edges:
-        add_edge(
-            graph_adjacency_list=graph,
-            edge_weights=weight,
-            edge_source=source,
-            edge_target=target,
-            weight_function=euclidean_distance,
-        )
-
-    # graph must have 4 nodes
-    assert len(graph) == 4
-
-    # there should be no edge between (0.0, 0.0) and (1.0, 1.0)
-    assert (1.0, 1.0) not in graph[(0.0, 0.0)]
-
-    # weight of (0.0, 0.0) - (1.0, 0.0) should be 1.0
-    assert weight[(0.0, 0.0)][(1.0, 0.0)] == 1.0
 
 
 def test_add_edge_method(square_edges):
@@ -80,37 +48,6 @@ def test_add_edge_method(square_edges):
 
     # weight of edge (0.0, 0.0) - (1.0, 0.0) should be 1.0
     assert graph.weight[(0.0, 0.0)][(1.0, 0.0)] == 1.0
-
-
-def test_add_edge_assertion_error(square_edges):
-    """
-    function should throw an assertion error when trying
-    to add edge that already exists.
-    """
-    # create data structure for graph, edge weights and edges
-    graph = defaultdict(set)
-    weight = defaultdict(dict)
-
-    # define graph from edges
-    for source, target in square_edges:
-        add_edge(
-            graph_adjacency_list=graph,
-            edge_weights=weight,
-            edge_source=source,
-            edge_target=target,
-            weight_function=euclidean_distance,
-        )
-
-    # try adding existing edge
-    source, target = (0.0, 0.0), (1.0, 0.0)
-    with pytest.raises(AssertionError, match="already exists"):
-        add_edge(
-            graph_adjacency_list=graph,
-            edge_weights=weight,
-            edge_source=source,
-            edge_target=target,
-            weight_function=euclidean_distance,
-        )
 
 
 @pytest.mark.parametrize(
@@ -144,40 +81,6 @@ def test_add_edge_method_assertion_error(
         )
 
 
-def test_remove_edge(square_edges):
-    """
-    Test edge removal.
-    """
-    # create data structure for graph, edge weights and edges
-    graph_adjacency_list = defaultdict(set)
-    edge_weights = defaultdict(dict)
-
-    # define graph from edges
-    for source, target in square_edges:
-        add_edge(
-            graph_adjacency_list,
-            edge_weights,
-            source,
-            target,
-            euclidean_distance,
-        )
-
-    # remove edge
-    edge_source, edge_target = (0.0, 0.0), (1.0, 0.0)
-    remove_edge(
-        graph_adjacency_list,
-        edge_weights,
-        edge_source,
-        edge_target,
-    )
-
-    # there must be no edge (0.0, 0.0) - (0.0, 1.0)
-    assert edge_target not in graph_adjacency_list[edge_source]
-
-    # there must be no weight associated with the edge (0.0, 0.0) - (0.0, 1.0)
-    assert edge_target not in edge_weights[edge_source]
-
-
 @pytest.mark.parametrize(
     "edge_source,edge_target",
     [((0.0, 0.0), (0.0, 1.0)), ((0.0, 1.0), (0.0, 0.0))],
@@ -208,45 +111,6 @@ def test_remove_edge_method(square_edges, edge_source, edge_target):
 
     # there must be no weight associated with the edge
     assert edge_target not in graph.weight[edge_source]
-
-
-def test_remove_edge_assertion_error(square_edges):
-    """
-    Function should throw an assertion error when
-    trying to remove an edge that does not exist in
-    the graph.
-    """
-    # create data structure for graph, edge weights and edges
-    graph_adjacency_list = defaultdict(set)
-    edge_weights = defaultdict(dict)
-
-    # define graph from edges
-    for source, target in square_edges:
-        add_edge(
-            graph_adjacency_list,
-            edge_weights,
-            source,
-            target,
-            euclidean_distance,
-        )
-
-    # remove edge
-    edge_source, edge_target = (0.0, 0.0), (1.0, 0.0)
-    remove_edge(
-        graph_adjacency_list,
-        edge_weights,
-        edge_source,
-        edge_target,
-    )
-
-    # try to remove nonexistent edge
-    with pytest.raises(AssertionError, match="No edge"):
-        remove_edge(
-            graph_adjacency_list,
-            edge_weights,
-            edge_source,
-            edge_target,
-        )
 
 
 @pytest.mark.parametrize(
@@ -285,46 +149,48 @@ def test_remove_edge_method_assertion_error(
         )
 
 
-def test_shortest_path(square_edges):
+@pytest.mark.parametrize(
+    "edge_source,edge_target,expected_dist",
+    [
+        ((0.0, 0.0), (0.0, 1.0), 1.0),
+        ((0.0, 0.0), (1.0, 1.0), 2.0),
+        ((0.0, 0.0), (0.25, 0.25), float("inf")),
+        ((0.0, 0.0), (0.75, 0.75), float("inf")),
+    ],
+)
+def test_dijkstra_algorithm(
+    square_edges, edge_source, edge_target, expected_dist
+):
     """
-    Tests to get the shortest path between nodes.
+    Test shortest path dijkstra algorithm. Distance/cost is infinite when
+    there is no path between nodes.
     """
-    # create data structure for graph, edge weights and edges
-    graph_adjacency_list = defaultdict(set)
-    edge_weights = defaultdict(dict)
+    # create instance of graph class
+    graph = Graph()
 
     # define graph from edges
     for source, target in square_edges:
-        add_edge(
-            graph_adjacency_list,
-            edge_weights,
-            source,
-            target,
-            euclidean_distance,
+        graph.add_edge(
+            edge_source=source,
+            edge_target=target,
+            edge_weight=euclidean_distance(source, target),
         )
 
-    # get the shortest path between nodes
-    edge_source, edge_target = (0.0, 0.0), (1.0, 0.0)
-    path = shortest_path(
-        graph_adjacency_list, edge_weights, edge_source, edge_target
+    # add edge to make the graph disconnected
+    source, target = (0.25, 0.25), (0.75, 0.75)
+    graph.add_edge(
+        edge_source=source,
+        edge_target=target,
+        edge_weight=euclidean_distance(source, target),
     )
 
-    # there is edge connecting the nodes, so the shortest path is formed by the nodes
-    # themselves.
-    assert path == [edge_source, edge_target]
+    # get the shortest path distances
+    distances, _ = dijkstra_algorithm(
+        graph=graph, edge_source=edge_source, edge_target=edge_target
+    )
 
-    # removing the edge, the shortest path will be formed by all the points,
-    # as it contains all other remaining edges.
-    remove_edge(
-        graph_adjacency_list,
-        edge_weights,
-        edge_source,
-        edge_target,
-    )
-    path = shortest_path(
-        graph_adjacency_list, edge_weights, edge_source, edge_target
-    )
-    assert path == [(0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0)]
+    # when there is no path between nodes, expected distance is inf.
+    assert distances[edge_target] == expected_dist
 
 
 def test_shortest_path_to_graph_class(square_edges):
@@ -362,57 +228,6 @@ def test_shortest_path_to_graph_class(square_edges):
         graph=graph, edge_source=edge_source, edge_target=edge_target
     )
     assert path == [(0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0)]
-
-
-def test_shortest_path_assertion_error(square_edges):
-    """
-    Function throws assertion error in two cases: when there is no path in
-    the graph connecting the two points or when one of the nodes (or both) are
-    not in the graph.
-    """
-    # create data structure for graph, edge weights and edges
-    graph_adjacency_list = defaultdict(set)
-    edge_weights = defaultdict(dict)
-
-    # define graph from edges
-    for source, target in square_edges:
-        add_edge(
-            graph_adjacency_list,
-            edge_weights,
-            source,
-            target,
-            euclidean_distance,
-        )
-
-    # add edge to make the graph disconnected
-    edge_source, edge_target = (0.25, 0.25), (0.75, 0.75)
-    add_edge(
-        graph_adjacency_list,
-        edge_weights,
-        edge_source,
-        edge_target,
-        euclidean_distance,
-    )
-
-    # Try to find the shortest path between nodes of one connected component and another
-    # (does not exist)
-    with pytest.raises(AssertionError, match="There is no path"):
-        shortest_path(
-            graph_adjacency_list=graph_adjacency_list,
-            edge_weights=edge_weights,
-            edge_source=(0.0, 0.0),
-            edge_target=edge_target,
-        )
-
-    # Try to find the shortest path between nodes that do not belong to the
-    # graph (impossible)
-    with pytest.raises(AssertionError, match="Impossible to find path"):
-        shortest_path(
-            graph_adjacency_list=graph_adjacency_list,
-            edge_weights=edge_weights,
-            edge_source=(11.0, 11.0),
-            edge_target=edge_target,
-        )
 
 
 def test_shortest_path_to_graph_class_assertion_error(square_edges):
